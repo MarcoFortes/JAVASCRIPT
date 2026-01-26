5c4dbe2153f722355e51490785babf2e86ee8e2b
Marco Fortes <mpfortes@uta.cv>
2026-01-26 22:12:08 -0100

    arrow functions

---
+++ fundamentos/funcoes/arrowFunctions.js
@@ -0,0 +1,21 @@
+
+
+
+// Arrow funcion 1
+const imprimirMsg = (msg) => console.log(msg);
+
+imprimirMsg('Hi Guys');   // invocando a função
+
+
+
+// Arrow function 2
+const soma = (a, b) => {
+    return a + b
+};
+
+
+
+
+
+
+
---
+++ fundamentos/funcoes/funcoesTradicionais.js
@@ -0,0 +1,11 @@
+
+
+function retornaTipo(tipo){
+    return tipo;
+}
+
+
+// vai imprimir o tipo da variável passada na função
+console.log(typeof(retornaTipo(null)));
+
+
